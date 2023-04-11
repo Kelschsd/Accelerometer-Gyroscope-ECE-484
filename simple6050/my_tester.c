d29c52bf1699c33ca5d516f3b93bfd92c5713a25
Samuel Kelsch <123428419+Kelschsd@users.noreply.github.com>
2023-04-11 18:18:45 -0400

    Add files via upload

---
+++ simple6050/my_tester.c
@@ -0,0 +1,20 @@
+// Basic demo for accelerometer readings from Adafruit MPU6050
+// Modified by Samuel Kelsch to test custom library simple6050
+
+#include "libs/simple6050.c"
+#include <util/delay.h>
+
+void setup(void) {
+  simple6050_setup();
+}
+
+int main() {
+	while(1){
+		/* Get new sensor events with the readings */
+		//simple6050_read();
+		/* Get new sensor events and print the readings */
+		simple6050_test();
+		_delay_ms(10);
+	}
+	return 0;
+}
